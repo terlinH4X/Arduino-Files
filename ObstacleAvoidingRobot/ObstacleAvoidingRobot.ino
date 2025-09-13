fc1ffdf6d4e7872ec699d7fc2c49e864e640c48b
terlinH4x <aawweb23@gmail.com>
2025-09-13 04:06:48 +0100

    Obstacle Avoiding Robot -- updated

--- ObstacleAvoidingRobot/ObstacleAvoidingRobot.ino
+++ ObstacleAvoidingRobot/ObstacleAvoidingRobot.ino
@@ -1,23 +1,142 @@
-
+#include <AFMotor.h>
+#include <NewPing.h>
 #include <Servo.h>
 
-Servo myservo;  // create Servo object to control a servo
-// twelve Servo objects can be created on most boards
+#define TRIG_PIN A0
+#define ECHO_PIN A2
+#define MAX_DISTANCE 200
+#define MAX_SPEED 255
 
-int pos = 0;  // variable to store the servo position
+NewPing radar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE);
+
+AF_DCMotor motorLeft(2);   // M2 = Left motor
+AF_DCMotor motorRight(4);  // M4 = Right motor
+Servo myservo;
+
+int distance = 100;
+int pos = 0;
 
 void setup() {
-	myservo.attach(9, 500, 2500);  // attaches the servo on pin 9 to the Servo object
+  motorLeft.setSpeed(MAX_SPEED);
+  motorRight.setSpeed(MAX_SPEED);
+
+  myservo.attach(9);
+  myservo.write(90);
+  delay(2000);
+
+  // Take initial distance readings
+  distance = checkDistance();
+  delay(100);
+  distance = checkDistance();
+  delay(100);
+  distance = checkDistance();
+  delay(100);
+  distance = checkDistance();
+  delay(100);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-	for (pos = 0; pos <= 180; pos += 1) {  // goes from 0 degrees to 180 degrees
-		// servo.write(pos);  // tell servo to go to position in variable 'pos'
-		delay(15);           // waits 15 ms for the servo to reach the position
-	}
-	for (pos = 180; pos >= 0; pos -= 1) {  // goes from 180 degrees to 0 degrees
-		myservo.write(pos);                  // tell servo to go to position in variable 'pos'
-		delay(15);                           // waits 15 ms for the servo to reach the position
-	}
-	// delay(5000);
+  int distanceRight = 0;
+  int distanceLeft = 0;
+  delay(40);
+
+  if (distance <= 50) {
+    stopMovement();
+    delay(100);
+    moveBackward();
+    delay(300);
+    stopMovement();
+    delay(200);
+
+    distanceRight = checkRightDistance();
+    delay(200);
+    distanceLeft = checkLeftDistance();
+    delay(200);
+
+    if (distanceRight >= distanceLeft) {
+      turnRight();
+      stopMovement();
+    } else {
+      turnLeft();
+      stopMovement();
+    }
+  } else {
+    moveForward();
+  }
+
+  distance = checkDistance();
+  Serial.println(distance);
+}
+
+// --- Distance Checking Functions ---
+int checkRightDistance() {
+  for (pos = 90; pos >= 0; pos--) {
+    myservo.write(pos);
+    delay(5);
+  }
+  myservo.write(0);
+  delay(500);
+  int distance = checkDistance();
+  delay(100);
+  for (pos = 0; pos <= 90; pos++) {
+    myservo.write(pos);
+    delay(5);
+  }
+  myservo.write(90);
+  return distance;
+}
+
+int checkLeftDistance() {
+  for (pos = 90; pos <= 180; pos++) {
+    myservo.write(pos);
+    delay(5);
+  }
+  myservo.write(180);
+  delay(500);
+  int distance = checkDistance();
+  delay(100);
+  for (pos = 180; pos >= 90; pos--) {
+    myservo.write(pos);
+    delay(5);
+  }
+  myservo.write(90);
+  return distance;
+}
+
+int checkDistance() {
+  delay(70);
+  int cm = radar.ping_cm();
+  if (cm == 0) cm = 250;
+
+  return cm;
+}
+
+// --- Motor Control Functions ---
+void stopMovement() {
+  motorLeft.run(RELEASE);
+  motorRight.run(RELEASE);
+}
+
+void moveForward() {
+  motorLeft.run(FORWARD);
+  motorRight.run(FORWARD);
+}
+
+void moveBackward() {
+  motorLeft.run(BACKWARD);
+  motorRight.run(BACKWARD);
+}
+
+void turnLeft() {
+  motorLeft.run(BACKWARD);
+  motorRight.run(FORWARD);
+  delay(300);
+}
+
+void turnRight() {
+  motorLeft.run(FORWARD);
+  motorRight.run(BACKWARD);
+  delay(300);
 }
